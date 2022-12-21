17280a0de74525edee52cb5e9cdcbbffb56bd2a9
şira nur yeşilırmak <siranur_03@hotmail.com>
2022-12-21 17:48:13 +0300

    added components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ToDos from './components/ToDos';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <ToDos></ToDos>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ToDos/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from './Footer';
+import Header from './Header';
+import Main from './Main';
+
+
+
+function ToDos() {
+  return (
+    <div>
+      <h1>ToDos</h1>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default ToDos
